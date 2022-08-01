629a319a2296006f632b3e4b4714572ee11841b1
Arlindo Vinicius Amado Nascimento Silva <viniciusamado7@gmail.com>
2022-07-31 23:58:29 -0300

    Projeto Final

--- modulo3/projeto-labeddit/src/components/Header/Header.jsx
+++ modulo3/projeto-labeddit/src/components/Header/Header.jsx
@@ -1,11 +1,14 @@
 import logo from "../../assets/logo-header.svg"
 import { HeaderContainer } from "./styled"
 import { goToFeedPage, goToLoginPage } from "../../routes/coordinator"
-import { Button } from "@mui/material"
-import { useNavigate } from "react-router-dom"
+import { Button, IconButton } from "@mui/material"
+import { useNavigate, useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
+import { NavigateBefore } from "@mui/icons-material";
+
 
 const Header = () => {
+    const params = useParams()
     const navigate = useNavigate()
     const [token, setToken] = useState()
 
@@ -21,18 +24,19 @@ const Header = () => {
     }
 
     return(
-        <HeaderContainer>            
+        <HeaderContainer>
+            {params && params.id ? <IconButton id="left" onClick={() => navigate(-1)}> <NavigateBefore/> </IconButton> : null}            
             <img src={logo} alt="logo" onClick={() => goToFeedPage(navigate)}/>
             {(token && token.length > 0) 
             ?
-            <Button 
+            <Button id="right"
             onClick={logOut}
             >Sair</Button>
             :
-            <Button
+            <Button id="right"
             onClick={() => {goToLoginPage(navigate)}}
             >
-                Entrar
+            Entrar
             </Button>
         }
         </HeaderContainer>
--- modulo3/projeto-labeddit/src/components/Header/styled.jsx
+++ modulo3/projeto-labeddit/src/components/Header/styled.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components"
 export const HeaderContainer = styled.div`
     display: grid;
     grid-template-columns: repeat(3, 1fr);
-    grid-template-areas: '. logo button';
+    grid-template-areas: 'back logo button';
     background-color: #EDEDED;
     width: 100vw;
     align-items: center;
@@ -14,9 +14,14 @@ export const HeaderContainer = styled.div`
         grid-area: logo;
         margin: 0 auto;
     }
-    & > button{
+    #right{
         grid-area: button;        
         right: 0;
         justify-self: right;
+    }
+    #left{
+        grid-area: back;
+        justify-self: left;
+
     }
 `
