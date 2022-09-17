038af0502527724f7b06e09938ab326576efc47d
Arlindo Vinicius Amado Nascimento Silva <viniciusamado7@gmail.com>
2022-09-17 13:46:40 -0300

    Versao final do projeto

--- modulo4/projeto-cookenu-back/tables.sql
+++ modulo4/projeto-cookenu-back/tables.sql
@@ -1,25 +1,23 @@
-CREATE TABLE IF NOT EXISTS to_do_list_users (
-    id VARCHAR(64) PRIMARY KEY,
-    name VARCHAR(64) NOT NULL,
-    nickname VARCHAR(64) NOT NULL,
-    email VARCHAR(64) NOT NULL, 
-    password VARCHAR(255) NOT NULL
+CREATE TABLE IF NOT EXISTS cookenu_user (
+	id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role ENUM("NORMAL", "ADMIN") DEFAULT "NORMAl"
 );
 
-CREATE TABLE IF NOT EXISTS to_do_list_tasks (
-    id VARCHAR(64) PRIMARY KEY,
-    title VARCHAR(64) NOT NULL,
-    description VARCHAR(1024) DEFAULT "No description provided",
-    deadline DATE,
-    status ENUM("TO_DO", "DOING", "DONE") DEFAULT "TO_DO",
-    author_id VARCHAR(64),
-    FOREIGN KEY (author_id) REFERENCES to_do_list_users(id)
+CREATE TABLE IF NOT EXISTS cookenu_follower (
+	follower_id VARCHAR(255) NOT NULL,
+    FOREIGN KEY (follower_id) REFERENCES cookenu_user(id),
+	following_id VARCHAR(255) NOT NULL,
+    FOREIGN KEY (following_id) REFERENCES cookenu_user(id)
 );
 
-CREATE TABLE IF NOT EXISTS to_do_list_assignees (
-    task_id VARCHAR(64),
-    assignee_id VARCHAR(64),
-    PRIMARY KEY (task_id, assignee_id),
-    FOREIGN KEY (task_id) REFERENCES to_do_list_tasks(id),
-    FOREIGN KEY (assignee_id) REFERENCES to_do_list_users(id)
+CREATE TABLE IF NOT EXISTS cookenu_recipe (
+	id VARCHAR(255) PRIMARY KEY,
+    creator_id VARCHAR(255) NOT NULL,
+	FOREIGN KEY (creator_id) REFERENCES cookenu_user(id),
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    created_at DATE NOT NULL
 );
